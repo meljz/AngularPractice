6d54449afda5fe769f55d0a5ecc3bbc6d233f426
Jay <reyesmeljohnjay@gmail.com>
2025-12-09 16:01:32 +0800

    added HTTP calls

--- src/app/services/todos.service.ts
+++ src/app/services/todos.service.ts
@@ -1,16 +1,20 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { Todo } from '../model/todo.type'; //this came from model/todo.type.ts
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodosService {
+
+  http = inject(HttpClient);
+/*
   todoItems: Array<Todo> = [{
     userId: 1,
     completed: false,
     title: 'Meljohn',
     id: 0,
-  },
+  }, 
   {
     userId: 1,
     completed: false,
@@ -24,5 +28,10 @@ export class TodosService {
     id: 2,
   },
 ];
-  constructor() { }
+  constructor() { }*/
+  getTodosFromAPI(){
+    const url = 'https://jsonplaceholder.typicode.com/todos/';
+    return this.http.get<Array<Todo>>(url);
+  }
+
 }
